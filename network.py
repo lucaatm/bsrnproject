d10aab25fd99bb55f15a2305fe69c70a4372a67e
SohalHaidari <shaidari465@gmail.com>
2025-05-23 13:13:20 +0200

    network.py hinzugefügt

---
+++ network.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+
+PORT = 5001
+BUFFER_SIZE = 1024
+
+def receive_messages(callback, port=PORT):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('', port))
+    print(f"[Netzwerk] Lausche auf UDP-Port {port}")
+    while True:
+        try:
+            data, addr = sock.recvfrom(BUFFER_SIZE)
+            message = data.decode('utf-8')
+            print(f"[Empfangen] {addr}: {message}")
+            callback(f"{addr[0]}: {message}")
+           
+        
+        except Exception as e:
+            print(f"[Fehler] {e}")
+            break
+
+def send_message(target_ip, target_port, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(message.encode('utf-8'), (target_ip, target_port))
+    sock.close()
